08beea06525f6516f3a7a85e5f487f83d7f34372
Nick <nicknchambers@gmail.com>
2016-09-06 15:54:23 -0600

    Added event listener to random button with random hex code background generation

--- scripts.js
+++ scripts.js
@@ -1,41 +1,23 @@
-
-
-// //function to set background to white
-// function resetBackground () {
-// $("body").css("background", "white");
-// };
-
 //function to set background to random color
-
-
-
-//event listener for reset button click
-$('#reset').click(function(){
+function resetBackground() {
   $("body").css("background", "white");
-});
-
+};
 //function to generate random hex code
-
 function getRandomHexCode () {
   var randomColor = "#"+((1<<24)*Math.random()|0).toString(16);
-  return randomColor;
   if (randomColor === "#FFFFFF") {
     var randomColor = "#"+((1<<24)*Math.random()|0).toString(16);
+    $("body").css("background", randomColor);
+  }
+  else {
+    $("body").css("background", randomColor);
   }
 };
-
-
-
-
 //event listener to random button click
-// $('#random').click(function(){
-//   getRandomHexCode;
-//   alert: "Hi!";
-// };
-
-
-
-
-//   $("body").css("background", randomColor);
-// });
-// });
+$('#random-color').click(function(){
+  getRandomHexCode();
+});
+//event listener for reset button click
+$('#reset').click(function(){
+  resetBackground();
+});
