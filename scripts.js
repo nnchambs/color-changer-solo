a9fa2fbbb92b49ce8758576ec10d57bc0c90c8c8
Nick <nicknchambers@gmail.com>
2016-09-06 14:50:24 -0600

    Established reset button functionality with event listener on reset button

---
+++ scripts.js
@@ -0,0 +1,19 @@
+
+
+//function to set background to white
+// function resetBackground () {
+// $(".body").css("background", "white");
+// };
+
+
+
+//event listener for reset button click
+$('#reset').click(function(){
+  $("body").css("background", "white");
+});
+
+//event listener to random button click
+$('#random').click(function(){
+  $("body").css()
+});
+//function to set background to random color
