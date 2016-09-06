d31259692fdce47c7d9b019aabd2977915a0baec
Nick <nicknchambers@gmail.com>
2016-09-06 15:31:23 -0600

    Added function to generate random hex code (getRandomHexCode)

--- scripts.js
+++ scripts.js
@@ -1,9 +1,11 @@
 
 
-//function to set background to white
+// //function to set background to white
 // function resetBackground () {
-// $(".body").css("background", "white");
+// $("body").css("background", "white");
 // };
+
+//function to set background to random color
 
 
 
@@ -12,8 +14,28 @@ $('#reset').click(function(){
   $("body").css("background", "white");
 });
 
+//function to generate random hex code
+
+function getRandomHexCode () {
+  var randomColor = "#"+((1<<24)*Math.random()|0).toString(16);
+  return randomColor;
+  if (randomColor === "#FFFFFF") {
+    var randomColor = "#"+((1<<24)*Math.random()|0).toString(16);
+  }
+};
+
+
+
+
 //event listener to random button click
-$('#random').click(function(){
-  $("body").css()
-});
-//function to set background to random color
+// $('#random').click(function(){
+//   getRandomHexCode;
+//   alert: "Hi!";
+// };
+
+
+
+
+//   $("body").css("background", randomColor);
+// });
+// });
